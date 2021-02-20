dc1c581f109ec351695816f64c8f6c1f1ae92242
leo-jansen <janseleo@gmail.com>
2021-02-20 15:28:59 -0300

    conserto do caminho do driver

--- src/insta_bot.py
+++ src/insta_bot.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import platform
-
+import os
 
 class InstagramBot:
   def __init__(self, username, password, link):
@@ -12,8 +12,9 @@ class InstagramBot:
 
   @staticmethod
   def escolher_driver():
+    caminho = os.path.dirname(os.path.realpath(__file__))  
     if platform.system() == "Windows":
-      web_driver = webdriver.Firefox("../driver/geckodriver-v0.29.0-win64/geckodriver.exe")
+      web_driver = webdriver.Firefox(executable_path=caminho+"/driver/geckodriver-v0.29.0-win64/geckodriver.exe")
     elif platform.system() == "Linux":
-      web_driver = webdriver.Firefox("../driver/geckodriver-v0.29.0-linux64/geckodriver")
+      web_driver = webdriver.Firefox(executable_path=caminho+"/driver/geckodriver-v0.29.0-linux64/geckodriver")
     return web_driver
