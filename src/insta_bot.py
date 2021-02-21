e207e27507702ac64aa5d1fc037376b7f0631c7e
leo-jansen <janseleo@gmail.com>
2021-02-21 14:27:33 -0300

    metodo login

--- src/insta_bot.py
+++ src/insta_bot.py
@@ -2,12 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import platform
 import os
+import time
 
 class InstagramBot:
-  def __init__(self, username, password, link):
+  def __init__(self, username, password, sorteio):
       self.username = username
       self.password = password
-      self.link = link
+      self.sorteio = sorteio
+      self.instagram = "https://www.instagram.com/"
       self.driver = InstagramBot.escolher_driver()
 
   @staticmethod
@@ -18,3 +20,18 @@ class InstagramBot:
     elif platform.system() == "Linux":
       web_driver = webdriver.Firefox(executable_path=caminho+"/driver/geckodriver-v0.29.0-linux64/geckodriver")
     return web_driver
+
+  def login(self):
+    driver = self.driver
+    driver.get(self.instagram)
+    time.sleep(2)
+    campo_usuario = driver.find_element_by_xpath("//input[@name='username']")
+    campo_usuario.click()
+    campo_usuario.clear()
+    campo_usuario.send_keys(self.username)
+    campo_senha = driver.find_element_by_xpath("//input[@name='password']")
+    campo_senha.click()
+    campo_senha.clear()
+    campo_senha.send_keys(self.password)
+    campo_senha.send_keys(Keys.RETURN)
+
