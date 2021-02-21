e90192565f7c516080e347aa9d3f39db4d3460c5
leo-jansen <janseleo@gmail.com>
2021-02-21 16:03:13 -0300

    conserto repeticao

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,15 @@
-from src.insta_bot import InstagramBot
+from insta_bot import InstagramBot
 
 usuario = input("Digite seu usuario: ")
 senha = input("Digite sua senha: ")
 sorteio = input("Digite o link do sorteio: ")
-numero_de_comentarios = input("Quantos comentarios deseja ter?")
+numero_de_comentarios = int(input("Quantos comentarios deseja ter? "))
 lista_comentario = []
-for comentario in numero_de_comentarios:
-  comentario = input("Digite o seu comentario")
+numero = 0
+while numero < numero_de_comentarios:
+  comentario = input("Digite o seu comentario: ")
   lista_comentario.append(comentario)
+  numero += 1
 
 bot = InstagramBot(usuario, senha, sorteio, lista_comentario)
 bot.login()
