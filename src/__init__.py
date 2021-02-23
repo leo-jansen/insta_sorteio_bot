99c4c5f17bfdf20d586ffd9a33732756494c118a
leo-jansen <janseleo@gmail.com>
2021-02-23 11:33:27 -0300

    executar com 2 click

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,16 @@
 from insta_bot import InstagramBot
 
-usuario = input("Digite seu usuario: ")
-senha = input("Digite sua senha: ")
-sorteio = input("Digite o link do sorteio: ")
-numero_de_comentarios = int(input("Quantos comentarios deja ter? "))
-lista_comentario = []
-comentario = input("Digite o seu comentario: ")
-lista_comentario.append(comentario)
-bot = InstagramBot(usuario, senha, sorteio, lista_comentario)
-bot.login()
-bot.comentario(numero_de_comentarios)
+def main():
+  usuario = input("Digite seu usuario: ")
+  senha = input("Digite sua senha: ")
+  sorteio = input("Digite o link do sorteio: ")
+  numero_de_comentarios = int(input("Quantos comentarios deja ter? "))
+  lista_comentario = []
+  comentario = input("Digite o seu comentario: ")
+  lista_comentario.append(comentario)
+  bot = InstagramBot(usuario, senha, sorteio, lista_comentario)
+  bot.login()
+  bot.comentario(numero_de_comentarios)
+
+if __name__ == "__main__":
+  main()
